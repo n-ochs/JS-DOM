23047babd31c5c02208ef8934dde4dfbd70627af
Nicholas Ochs <nick.ochs@yahoo.com>
2020-09-26 12:28:09 -0400

    adding comments to 7

--- ex7/script.js
+++ ex7/script.js
@@ -7,9 +7,9 @@ let section = document.querySelector('#section');
 
 sectionHeader.addEventListener('click', function() {
     if (section.style.display === 'none') {
-        section.style.display = 'block';
+        section.style.display = 'block'; //if section does not have a display style already, set the display style to block.
     }
     else {
-        section.style.display = 'none';
+        section.style.display = 'none'; //it is already changed to block, so therefore the first if will not run. the else will run now, setting it back to none.
     }
 })
